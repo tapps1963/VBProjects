abc70c1f86a7d84fc84c98f48831b7c325abf1c9
Shayne Chadwick Tappan <Shayne.Tappan@eoh.co.za>
2018-11-18 17:48:58 +0200

    Second Hello World

---
+++ 002 Hello World 2/002 Hello World 2/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdHello = New System.Windows.Forms.Button()
+        Me.txtHelloWorld = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmdHello
+        '
+        Me.cmdHello.Location = New System.Drawing.Point(19, 34)
+        Me.cmdHello.Name = "cmdHello"
+        Me.cmdHello.Size = New System.Drawing.Size(107, 34)
+        Me.cmdHello.TabIndex = 0
+        Me.cmdHello.Text = "Hello"
+        Me.cmdHello.UseVisualStyleBackColor = True
+        '
+        'txtHelloWorld
+        '
+        Me.txtHelloWorld.Location = New System.Drawing.Point(154, 34)
+        Me.txtHelloWorld.Multiline = True
+        Me.txtHelloWorld.Name = "txtHelloWorld"
+        Me.txtHelloWorld.Size = New System.Drawing.Size(147, 34)
+        Me.txtHelloWorld.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(329, 93)
+        Me.Controls.Add(Me.txtHelloWorld)
+        Me.Controls.Add(Me.cmdHello)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdHello As Button
+    Friend WithEvents txtHelloWorld As TextBox
+End Class
---
+++ 002 Hello World 2/002 Hello World 2/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub cmdHello_Click(sender As Object, e As EventArgs) Handles cmdHello.Click
+        txtHelloWorld.Text = "Hello World"
+    End Sub
+End Class
