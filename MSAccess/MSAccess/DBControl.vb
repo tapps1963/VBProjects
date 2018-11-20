d050fef01f4c17c64a4c531e15d0a4bef3e6493a
Shayne Chadwick Tappan <Shayne.Tappan@eoh.co.za>
2018-11-20 14:10:37 +0200

    Update Code

--- MSAccess/MSAccess/DBControl.vb
+++ MSAccess/MSAccess/DBControl.vb
@@ -2,7 +2,59 @@
 
 Public Class DBControl
     ' Create a DB Connection
-
     Private DBCon As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=QBankDB.accdb")
+
+    ' Create DB Command
+    Private DBCmd As OleDbCommand
+
+    ' DB Data
+    Public DBDA As OleDbDataAdapter
+    Public DBDT As DataTable
+
+    ' Query Parameters
+    Public Params As New List(Of OleDbParameter)
+
+    ' Query Statistics
+    Public RecordCount As Integer
+    Public Exception As String
+
+    Public Sub ExecQuery(Query As String)
+        ' Reset Query Statistics
+        RecordCount = 0
+        Exception = ""
+
+        Try
+            ' Open a Connection
+            DBCon.Open()
+
+            ' Create Database Command
+            DBCmd = New OleDbCommand(Query, DBCon)
+
+            ' Load Params into DBCom
+            Params.ForEach(Sub(p) DBCmd.Parameters.Add(p))
+
+            ' Clear Params list
+            Params.Clear()
+
+            ' Execute Command and fill dataset (DataTable)
+            DBDT = New DataTable
+            DBDA = New OleDbDataAdapter(DBCmd)
+
+            RecordCount = DBDA.Fill(DBDT)
+
+        Catch ex As Exception
+            Exception = ex.Message
+        End Try
+
+        ' Close the connection
+        If DBCon.State = ConnectionState.Open Then DBCon.Close()
+
+    End Sub
+
+    ' Include Query and command parameters
+    Public Sub AddParam(Name As String, Value As Object)
+        Dim newParam As New OleDbParameter(Name, Value)
+        Params.Add(newParam)
+    End Sub
 
 End Class
--- MSAccess/MSAccess/Form1.vb
+++ MSAccess/MSAccess/Form1.vb
@@ -1,3 +1,53 @@
 ﻿Public Class Form1
+    Private Access As New DBControl
+    Private Function NotEmpty(text As String) As Boolean
+        Return Not String.IsNullOrEmpty(text)
+    End Function
 
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        ' Run Query
+        Access.ExecQuery("select * from tbl_users order by first_name")
+
+        If NotEmpty(Access.Exception) Then
+            MsgBox(Access.Exception)
+            Exit Sub
+        End If
+
+        ' Fill DataGrid
+        dgvData.DataSource = Access.DBDT
+
+        ' Fill ComboBox
+        For Each R As DataRow In Access.DBDT.Rows
+            cbxUsers.Items.Add(R("user_login"))
+        Next
+
+        ' Display first name found
+        If Access.RecordCount > 0 Then
+            cbxUsers.SelectedIndex = 0
+
+        End If
+    End Sub
+
+    Private Sub SearchMember(Name As String)
+        ' Add Parameters
+        Access.AddParam("@user_login", "%" & Name & "%")
+        Access.ExecQuery("select * from tbl_users where user_login like @user_login")
+
+        ' Report and Abort
+        If NotEmpty(Access.Exception) Then MsgBox(Access.Exception) : Exit Sub
+
+        ' Fill DataGrid
+        dgvData.DataSource = Access.DBDT
+
+
+
+    End Sub
+
+    Private Sub cmdFind_Click(sender As Object, e As EventArgs) Handles cmdFind.Click
+        SearchMember(txtFind.Text)
+    End Sub
+
+    Private Sub txtFind_TextChanged(sender As Object, e As EventArgs) Handles txtFind.TextChanged
+        SearchMember(txtFind.Text)
+    End Sub
 End Class
